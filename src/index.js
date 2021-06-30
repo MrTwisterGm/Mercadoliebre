6fd13e48934c6579426ffb5d4bd41513b768df2c
MrTwisterGm <56003873+MrTwisterGm@users.noreply.github.com>
2021-06-30 13:38:40 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const express = require ("express")
-const app = express()
-const path = require ("path")
-//server
-app.set("port",process.env.PORT || 3000 )
-// app.listen(3000,()=>console.log("server on http://localhost:3000"))
+const express = require('express');
+const path = require('path');
+const app = express();
 
+app.use( express.static(path.resolve (__dirname,"./public")));
 
-app.listen(app.get("port"),() => console.log("Server Start http://localhost:"+app.get("port")))
+app.listen(process.env.PORT || 3020, () => console.log('esto fue exitoso'));
 
-//public uso de la carpeta publica
-app.use(express.static(path.resolve(__dirname,"../public")))
+app.get('/', (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/home.html"))
+});
 
-//website routes
-app.use (require("./routes/web"))
+app.get('/login', (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/login.html"))
+});
 
-
+app.get('/register', (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/register.html"))
+});
